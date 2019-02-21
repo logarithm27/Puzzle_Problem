6519960b498eb4efd127901312ef3e0a99924324
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-21 18:09:28 +0100

    commit

--- Ui.py
+++ Ui.py
@@ -1,58 +1,47 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-import numpy
+from PyQt5 import QtCore, QtWidgets
+from Numpy import matrix
 
 
 class UiForm(object):
     def setup_ui(self, form):
-        # number of blocks on the puzzle
         num_blocks = 3
-
-        # matrix that takes value from 1 to 10 (10 - 1 = 9 = 3²)
-        # reshape function takes first arg as number of lines and second as columns
-        # We want to build a matrix of a range equal to n²,
-        # hence, to reach n², we add 1 because we start from 1 and not 0
-        # max_range - start range = 10 - 1 = 9 = n²
-
-        max_range = numpy.square(num_blocks) + 1
-        final_puzzle_state = \
-            numpy.arange(1, max_range).reshape(num_blocks, num_blocks)
-
-        initial_puzzle_state = numpy.arange(1, max_range).reshape(num_blocks, num_blocks)
-        # get index of last element
-        index = numpy.where(initial_puzzle_state == max_range - 1)[0][0]
-        initial_puzzle_state[index][index] = -1
-        final_puzzle_state[index][index] = -1
-        # shuffling columns
-        for i in range(num_blocks):
-            numpy.random.shuffle(initial_puzzle_state[i])
-        # shuffling lines
-        numpy.random.shuffle(initial_puzzle_state)
+        gaming_block = matrix(num_blocks)
+        # next access to the values inside generator, 0 is the shuffled matrix,
+        # 1 is the goal state or ordered matrix, 2 is the number of blocks
+        initial_puzzle_state = next(gaming_block)[0]
         form.setObjectName("Puzzle Game AI")
         form.setWindowTitle("Puzzle Game AI")
+        # width and height of each button ( or block )
         width = 77
         height = 77
+        # limit the window's size to the number of blocks
+        # i.e : 77 * 3 = 231 ==> we'll have a windows of 231px x 231px
         form.resize(width * num_blocks, width * num_blocks)
-        self.buttons = []
-        for i in range(num_blocks):
-            self.buttons.append([])
+        # initialize matrix of buttons similar to the shuffled matrix
+        self.buttons = [[] for i in range(num_blocks)]
         y_coordinate = 0
         for y_position, i in enumerate(initial_puzzle_state):
+            # x coordinate set back to 0 each time we've done building the current line
             x_coordinate = 0
             if not y_position == 0:
                 y_coordinate += 80
+                # x_position is the index, j is the value on that index
+                # i.e : array[x_position] = j
             for x_position, j in enumerate(i):
+                # if button is the first element, then x coordinate will be set to 0
                 if not x_position == 0:
                     x_coordinate += 80
                 button = QtWidgets.QPushButton(form)
                 button.setGeometry(QtCore.QRect(x_coordinate, y_coordinate, width, height))
                 button.setStyleSheet("font: 22pt \"MS Shell Dlg 2\";\n"
                                         "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 178, 102, 255), stop:0.55 rgba(235, 148, 61, 255), stop:0.98 rgba(0, 0, 0, 255), stop:1 rgba(0, 0, 0, 0));")
+                # the object name of each button is the number generated in the shuffled matrix
                 button.setObjectName(str(j))
                 button.setText(str(j))
                 # hide the last element (last element should be the empty square)
                 if j == -1:
                     button.setVisible(False)
-                # self.buttons[y_position][x_position] = button
+                self.buttons[y_position].append(button)
 
 
 
--- Window.py
+++ Window.py
@@ -1,11 +1,10 @@
-# usr/bin/Omar/2019/NxN Puzzle
-
+# !usr/bin/Omar/2019/NxN Puzzle
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QRect, QPropertyAnimation
 from Ui import UiForm
+
 
 class MainWindow():
     def __init__(self):
@@ -14,10 +13,14 @@ class MainWindow():
         self.ui.setup_ui(self.main_win)
         self.main_win.setStyleSheet("background-color: rgb(178, 222, 124)")
         self.main_win.setFixedSize(self.main_win.width(), self.main_win.height())
-        # self.ui.buttons[2][2].clicked.connect(lambda : self.on_button_click(160, 160, self.ui.pushButton_8))
+        # TO DO : functions slot to animate the block's move
+
+        # self.ui.buttons[0][0].clicked.connect(lambda : self.on_button_click(160, 160, self.ui.pushButton_8))
 
     def show(self):
         self.main_win.show()
+
+    # TO DO : animate button's move
 
     def on_button_click(self, x_coordinate, y_coordinate, _button):
         self.anim = QPropertyAnimation(_button, b"geometry")
@@ -26,6 +29,7 @@ class MainWindow():
         self.anim.setEndValue(QRect(x_coordinate, y_coordinate, 77, 77))
         print('mooving')
         self.anim.start()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
