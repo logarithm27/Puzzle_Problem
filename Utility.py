b1e5de3f15d435ff690ec1356b5ce5e314b66656
Maftoul Omar <maftoul.omar@gmail.com>
2019-02-24 19:23:05 +0100

    Using argwhere instead of where adding BlanButton class

--- Utility.py
+++ Utility.py
@@ -43,28 +43,4 @@ class BlankButton:
     # pour avoir les voisins du boutton vide
 
     def get_neighbor_buttons_of_blank_button(self, matrix):
-        # si la case est vide se trouve au debut du taquin
-        if self.line_index == 0 and self.column_index == 0:
-            # avoir tous les voisins de la case vide qui se trouvent à droite
-            self.right_neighbors = matrix[self.line_index][self.column_index+1::]
-            # le premier voisin de la case vide à droite
-            self.right_neighbor = self.right_neighbors[0]
-            # tous les voisins du bas
-            for i in range(1, len(matrix)):
-                self.down_neighbors.apend(matrix[self.line_index+i][self.column_index])
-            # le voisin du bas
-            self.down_neighbor = self.down_neighbors[0]
-
-        # si la case est vide se trouve dans les derniers indices du taquin
-        if self.line_index == len(matrix)-1 and self.column_index == len(matrix)-1:
-            # voisins de gauche
-            self.left_neighbors = matrix[self.line_index][self.column_index-1::]
-            # voisin de gauche
-            self.left_neighbor = self.left_neighbors[0]
-            # tous les voisins du haut
-            for i in range(1, len(matrix)):
-                self.up_neighbors.apend(self.matrix[self.line_index-i][self.column_index])
-            self.up_neighbor = self.down_neighbors[0]
-
-            # TO DO FOR OTHER CASES (pas encore complet)
-
+       
