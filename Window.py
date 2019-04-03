d444f2d71d392666437d0ee88b0844f6a7dd791c
M. OMAR <maftoul.omar@gmail.com>
2019-04-03 11:47:19 +0200

    Update Window.py

--- Window.py
+++ Window.py
@@ -53,55 +53,15 @@ class MainWindow:
         self.ui.shuffle.clicked.connect(lambda: self.on_shuffle_click())
 
     def play_game(self):
-        # self.puzzle = Puzzle(self.instance)
-        # self.state = Play(self.puzzle)
-        # self.play = self.state.play()
-        self.expansed = 0
-        test = True
-        while test:
-            listeEtat = self.taquin.frontiere.popitem(0)
-            for etat in listeEtat[1]:
-                test, self.listeSolution = self.taquin.expanser(etat, listeEtat[0], 7, len(self.instance))
-                if not test:
-                    break
-            self.expansed += 1
-        self.moves = 0
-        self.states = []
-        for i, state in enumerate(reversed(self.listeSolution)):
-            self.states.append(state.matrice)
-            self.moves += 1
-        self.moving()
+         self.puzzle = Puzzle(self.instance)
+         self.state = Play(self.puzzle)
+         self.play = self.state.play()
+
 
     def show(self):
         self.main_win.update()
         self.main_win.show()
 
-    def moving(self):
-        self.indices = []
-        self.indices2 = []
-        self.indices3 = []
-        for state in self.states:
-            for line_index, column_index in itertools.product(range(len(self.instance)),
-                                                              range(len(self.instance))):
-                if state[line_index][column_index] == -1:
-                        self.indices.append((line_index, column_index))
-                        self.indices3.append((line_index, column_index))
-        self.indices.pop(0)
-        for state in self.states:
-            if self.indices:
-                ind2 = self.indices3.pop(0)
-                ind = self.indices.pop(0)
-                direcs = {' moved Right \u2192': (ind2[0], ind2[1]-1),
-                          ' moved Left \u2190': (ind2[0], ind2[1]+1),
-                          ' moved Down \u2193': (ind2[0]-1, ind2[1]),
-                          ' moved Up \u2191': (ind2[0]+1, ind2[1])}
-                ac = ''
-                for action, (line, column) in direcs.items():
-                    if (line, column) == ind:
-                        ac = action
-                        break
-                ac = str(state[ind[0]][ind[1]]) + ac
-                self.indices2.append((str(state[ind[0]][ind[1]]), ac))
 
     def on_button_click(self):
         self.move()
@@ -148,72 +108,7 @@ class MainWindow:
 
 
 if __name__ == '__main__':
-    # taille = input("Donnez la taille du taquin: ")
-    # taille = int(taille)
-    # heuristique = input("Donnez l'heuristique: ")
-    # heuristique = int(heuristique)
-    #
-    # testPerf6 = []
-    # testPerf7 = []
-    # testPerf8 = []
-    # testPerf9 = []
-    # for perf in range(10):
-    #     for i in range(6, 10):
-    #         if i==64:
-    #             continue
-    #         start_time = time.time()
-    #         print("Heuristique: ", i)
-    # taquin.frontiere = SortedDict()
-    # taquin.explorer = {}
-    # taquin.initialiser(7, taille)
-    # taquin.instance = taquin.creerInstanceAleatoire(taille)
-    #
-    # test = True
-    # while test:
-    #     listeEtat = taquin.frontiere.popitem(0)
-    #     for etat in listeEtat[1]:
-    #         test, listeSolution = taquin.expanser(etat, listeEtat[0], 7, taille)
-    #         if not test:
-    #             break
-    # if i == 6:
-    #     testPerf6.append(listeSolution[0].g_E)
-    # if i == 7:
-    #     testPerf7.append(listeSolution[0].g_E)
-    # if i == 8:
-    #     testPerf8.append(listeSolution[0].g_E)
-    # if i == 9:
-    #     testPerf9.append(listeSolution[0].g_E)
-    # print("Temps d execution : %s secondes ---" % (time.time() - start_time))
-    # pause = input("Appuyez pour continuer...")
-    # somme=0
-    # for element in testPerf6:
-    #     somme += element
-    # moyenne = somme / len(testPerf6)
-    # print('La moyenne de heuristique 6 vaut: ', moyenne)
-    # print('Max dans 6 = ', max(testPerf6))
-    #
-    # somme=0
-    # for element in testPerf7:
-    #     somme += element
-    # moyenne = somme / len(testPerf7)
-    # print('La moyenne de heuristique 7 vaut: ', moyenne)
-    #
-    # somme=0
-    # for element in testPerf8:
-    #     somme += element
-    # moyenne = somme / len(testPerf8)
-    # print('La moyenne de heuristique 8 vaut: ', moyenne)
-    #
-    # somme=0
-    # for element in testPerf9:
-    #     somme += element
-    # moyenne = somme / len(testPerf9)
-    # print('La moyenne de heuristique 9 vaut: ', moyenne)
-    num_blocks = 4
-    taquin = SolutionTaquin(num_blocks)
-    taquin.frontiere = SortedDict()
-    taquin.explorer = {}
-    taquin.initialiser(7, num_blocks)
+
     app = QApplication(sys.argv)
     main_window = MainWindow(taquin, taquin.instance)
     main_window.show()
